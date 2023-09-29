7500d67116e3b22661d86bad939eb4686c61fb65
Afnaan Ahmed <86115809+Afnaan-Ahmed@users.noreply.github.com>
2023-09-29 16:01:29 +0530

    added colors

--- nmap_payload_generator.py
+++ nmap_payload_generator.py
@@ -1,13 +1,43 @@
 import subprocess
 from time import sleep
 
+
+
+black = "\033[0;30m"
+red = "\033[0;31m"
+green = "\033[0;32m"
+brown = "\033[0;33m"
+blue = "\033[0;34m"
+purple = "\033[0;35m"
+cyan = "\033[0;36m"
+light_gray = "\033[0;37m"
+dark_gray = "\033[1;30m"
+light_red = "\033[1;31m"
+light_green = "\033[1;32m"
+yellow = "\033[1;33m"
+light_blue = "\033[1;34m"
+light_purple = "\033[1;35m"
+light_cyan = "\033[1;36m"
+light_white = "\033[1;37m"
+bold = "\033[1m"
+faint = "\033[2m"
+italic = "\033[3m"
+underline = "\033[4m"
+blink = "\033[5m"
+negative = "\033[7m"
+crossed = "\033[9m"
+reset = "\033[0m"
+
+
+
+
 def main_menu():
     subprocess.run("clear")
-    print("         MENU            ")
+    print(light_green + "         MENU            " + reset)
     print()
-    print("[1] - create payload")
-    print("[2] - see templates")
-    print("[3] - exit")
+    print(light_red + "[1] " +reset+ "- create payload")
+    print(light_blue + "[2] " +reset+ "- see templates")
+    print(light_red + "[3] " +reset+ "- exit")
     response = input("> ")
     if response == '1':
         create()
@@ -16,7 +46,7 @@ def main_menu():
     elif response == '3':
         print("Goodbye!")
     else:
-        print("Invalid Input")
+        print(light_red+"Invalid Input"+reset)
         main_menu()
 
 
@@ -24,17 +54,18 @@ def main_menu():
 def create():
     payload = "sudo nmap"
     subprocess.run("clear")
-    print("Choose a scan type:")
-    print('''[1] - TCP SYN port scan (Default)
-[2] - TCP connect port scan (Default without root privilege)
-[3] - UDP port scan
-[4] - TCP ACK port scan
-[5] - TCP Window port scan
-[6] - TCP Maimon port scan
-[7] - Skip''')
+    print(light_cyan +"Scan type:"+reset)
+    print(f'''
+{light_green}[1] {reset}- TCP SYN port scan (Default)
+{light_green}[2] {reset}- TCP connect port scan (Default without root privilege)
+{light_green}[3] {reset}- UDP port scan
+{light_green}[4] {reset}- TCP ACK port scan
+{light_green}[5] {reset}- TCP Window port scan
+{light_green}[6] {reset}- TCP Maimon port scan
+{light_green}[7] {reset}- Skip''')
     response = input("> ")
     if response not in ['1','2','3','4','5','6','7','8','9']:
-        print("Invalid input skipping...")
+        print(light_red+"Invalid input, skipping..."+reset)
         sleep(0.7)
 
     dictionary = {
@@ -49,23 +80,24 @@ def create():
     try:
         payload += ' '+dictionary[response]
     except:
-        print("Invalid Input")
+        print()
     
     subprocess.run("clear")
-    print("Host Discovery:")
-    print('''[1] - No Scan. List targets only
-[2] - Disable port scanning. Host discovery only.
-[3] - Disable host discovery. Port scan only.
-[4] - TCP SYN discovery on port x. Port 80 by default
-[5] - TCP ACK discovery on port x. Port 80 by default
-[6] - UDP discovery on port x. Port 40125 by default
-[7] - ARP discovery on local network
-[8] - Never Do DNS resolution
-[9] - Skip
+    print(light_cyan+"Host Discovery:"+reset)
+    print(f'''
+{light_green}[1] {reset}- No Scan. List targets only
+{light_green}[2] {reset}- Disable port scanning. Host discovery only.
+{light_green}[3] {reset}- Disable host discovery. Port scan only.
+{light_green}[4] {reset}- TCP SYN discovery on port x. Port 80 by default
+{light_green}[5] {reset}- TCP ACK discovery on port x. Port 80 by default
+{light_green}[6] {reset}- UDP discovery on port x. Port 40125 by default
+{light_green}[7] {reset}- ARP discovery on local network
+{light_green}[8] {reset}- Never Do DNS resolution
+{light_green}[9] {reset}- Skip
 ''')
     response = input("> ")
     if response not in ['1','2','3','4','5','6','7','8','9']:
-        print("Invalid input skipping...")
+        print(light_red+"Invalid input, skipping..."+reset)
         sleep(0.7)
 
     dictionary = {
@@ -82,18 +114,18 @@ def create():
     try:
         payload += ' '+dictionary[response]
     except:
-        print("Invalid Input")
+        print()
     
     subprocess.run("clear")
-    print("Service and Version Detection:")
-    print('''
-[1] - Detect OS 
-[2] - Detect Version of service
-[3] - Enables OS detection, version detection, script scanning, and traceroute
+    print(light_cyan + "Service and Version Detection:" + reset)
+    print(f'''
+{light_green}[1] {reset}- Detect OS 
+{light_green}[2] {reset}- Detect Version of service
+{light_green}[3] {reset}- Enables OS detection, version detection, script scanning, and traceroute
 ''')
     response = input("> ")
     if response not in ['1','2','3','4','5','6','7','8','9']:
-        print("Invalid input skipping...")
+        print(light_red+"Invalid input, skipping..."+reset)
         sleep(0.7)
 
     dictionary = {
@@ -104,21 +136,21 @@ def create():
     try:
         payload += ' '+dictionary[response]
     except:
-        print("Invalid Input")
+        print()
 
     subprocess.run("clear")
-    print("Scan Speed")
-    print('''
-[1] - Paranoid - Intrusion Detection System evasion
-[2] - Sneaky - Intrusion Detection System evasion
-[3] - Polite - slows down the scan to use less bandwidth and use less target machine resources
-[4] - Normal - which is default speed
-[5] - Aggressive - speeds scans; assumes you are on a reasonably fast and reliable network
-[6] - Insane - speeds scan; assumes you are on an extraordinarily fast network
+    print(light_cyan + "Scan Speed" + reset)
+    print(f'''
+{light_green}[1] {reset}- Paranoid - Intrusion Detection System evasion
+{light_green}[2] {reset}- Sneaky - Intrusion Detection System evasion
+{light_green}[3] {reset}- Polite - slows down the scan to use less bandwidth and use less target machine resources
+{light_green}[4] {reset}- Normal - which is default speed
+{light_green}[5] {reset}- Aggressive - speeds scans; assumes you are on a reasonably fast and reliable network
+{light_green}[6] {reset}- Insane - speeds scan; assumes you are on an extraordinarily fast network
 ''')
     response = input("> ")
     if response not in ['1','2','3','4','5','6','7','8','9']:
-        print("Invalid input skipping...")
+        print(light_red+"Invalid input, skipping..."+reset)
         sleep(0.7)
 
     dictionary = {
@@ -132,21 +164,21 @@ def create():
     try:
         payload += ' '+dictionary[response]
     except:
-        print("Invalid Input")
+        print()
     
 
     subprocess.run("clear")
-    print("Scripts:")
-    print('''
-[1] - Scan with default NSE scripts. Considered useful for discovery and safe
-[2] - Scan for vulnerabilities 
-[3] - Use only safe scripts
-[4] - Use intrusive scripts. Considered unsafe.
-[5] - Skip
+    print(light_cyan + "Scripts:" + reset)
+    print(f'''
+{light_green}[1] {reset}- Scan with default NSE scripts. Considered useful for discovery and safe
+{light_green}[2] {reset}- Scan for vulnerabilities 
+{light_green}[3] {reset}- Use only safe scripts
+{light_green}[4] {reset}- Use intrusive scripts. Considered unsafe.
+{light_green}[5] {reset}- Skip
 ''')
     response = input("> ")
     if response not in ['1','2','3','4','5','6','7','8','9']:
-        print("Invalid input skipping...")
+        print(light_red+"Invalid input, skipping..."+reset)
         sleep(0.7)
 
     dictionary = {
@@ -159,11 +191,11 @@ def create():
     try:
         payload += ' '+dictionary[response]
     except:
-        print("Invalid Input")
+        print()
 
     
     print()          
-    print("Your payload is: " + payload + " <IP address/range>")
+    print("Your payload is: "+light_green+ payload +" <IP address/range>" + reset)
     print("Replace <IP address/range>")
     print()
 
